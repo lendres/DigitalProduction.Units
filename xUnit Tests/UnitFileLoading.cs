5e3329f70e55d6c767add9d9a500a25e49466ccc
lendres <lanceendres@gmail.com>
2024-08-06 07:58:05 -0500

    Add new unit tests.

---
+++ xUnit Tests/UnitFileLoading.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thor.Units;
+
+namespace xUnitTests;
+
+public static class UnitFileLoading
+{
+	public static string	FileNameV1		= "Units v1.0.xml";
+	public static string	FileNameV2		= "Units v2.0.xml";
+
+	public static string Folder { get; set; } = "";
+
+	public static string PathV1 { get =>  Path.Combine(Folder, FileNameV1); }
+	public static string PathV2 { get =>  Path.Combine(Folder, FileNameV2); }
+
+	static UnitFileLoading()
+	{
+		Folder		= DigitalProduction.IO.Path.ChangeDirectoryDotDot(Directory.GetCurrentDirectory(), 4);
+		Folder		= Path.Combine(Folder, "Input Files");
+		Debug.WriteLine("Root folder: " + Folder);
+	}
+
+	public static UnitConverter LoadVersionOneFile()
+	{
+		UnitConverter	unitsConverter = (UnitConverter)InterfaceFactory.CreateUnitConverter();
+		unitsConverter.OnError  += new UnitEventHandler(Converter_OnError);
+
+		string path	= Path.Combine(Folder, FileNameV1);
+
+		Debug.WriteLine("File: " + path);
+
+		UnitResult result = unitsConverter.LoadUnitsFile(path);
+		Trace.Assert(result != UnitResult.FileNotFound);
+
+		return unitsConverter;
+	}
+
+	public static UnitConverter LoadVersionTwoFile()
+	{
+		string path	= Path.Combine(Folder, FileNameV2);
+
+		Debug.WriteLine("File: " + path);
+
+		return UnitConverter.Deserialize(path);
+	}
+
+	/// <summary>
+	/// Error handler for converter.
+	/// </summary>
+	/// <param name="sender">Sender.</param>
+	/// <param name="eventArgs">Event arguments.</param>
+	private static void Converter_OnError(object sender, Thor.Units.UnitEventArgs eventArgs)
+	{
+		Debug.WriteLine("Error: " + eventArgs.DetailMessage);
+	}
+}
--- xUnit Tests/UnitTest1.cs
+++ xUnit Tests/UnitTest1.cs
@@ -5,43 +5,19 @@ namespace xUnitTests;
 
 public class UnitTest1
 {
-	UnitConverter	_unitsConverter;
 	public UnitTest1()
 	{
-		_unitsConverter = (UnitConverter)InterfaceFactory.CreateUnitConverter();
-		_unitsConverter.OnError  += new UnitEventHandler(Converter_OnError);
-
-		Folder		= DigitalProduction.IO.Path.ChangeDirectoryDotDot(Directory.GetCurrentDirectory(), 4);
-		Folder		= Path.Combine(Folder, "Input Files");
-		string path	= Path.Combine(Folder, "Units v1.0.xml");
-
-		Debug.WriteLine("Root folder: " + Folder);
-		Debug.WriteLine("File: " + path);
-
-		UnitResult result = _unitsConverter.LoadUnitsFile(path);
-		Trace.Assert(result != UnitResult.FileNotFound);
 	}
 
-	private string Folder { get; set; }
-	
 	[Fact]
-	public void Test1()
+	public void VersionConversionTest()
 	{
-		string path = Path.Combine(Folder, "Units v2.0.xml");
-		_unitsConverter.Serialize(path);
+		UnitConverter unitConverterV1 = UnitFileLoading.LoadVersionOneFile();
 
-		UnitConverter unitsConverter = UnitConverter.Deserialize(path);
+		// Serialize as V2, then deserialize it.
+		unitConverterV1.Serialize(UnitFileLoading.PathV2);
+		UnitConverter unitConverterV2 = UnitFileLoading.LoadVersionTwoFile();
 
-		Assert.Equal(_unitsConverter.Groups.Count, unitsConverter.Groups.Count);
-	}
-
-	/// <summary>
-	/// Error handler for converter.
-	/// </summary>
-	/// <param name="sender">Sender.</param>
-	/// <param name="eventArgs">Event arguments.</param>
-	private static void Converter_OnError(object sender, Thor.Units.UnitEventArgs eventArgs)
-	{
-		throw new NotImplementedException();
+		Assert.Equal(unitConverterV1.Groups.Count, unitConverterV2.Groups.Count);
 	}
 }
