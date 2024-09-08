5228c696fa770865c99a6fcef9d8c512900ab001
lendres <lanceendres@gmail.com>
2024-09-08 08:21:56 -0700

    Update ConversionTests.cs

--- xUnit Tests/ConversionTests.cs
+++ xUnit Tests/ConversionTests.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,5 +31,26 @@ public class ConversionTests
 
 		Assert.Equal(UnitResult.NoError, result);
 		Assert.Equal(3.28083989501312, convertedValue, 0.0000001);
+	}
+
+	[Fact]
+	public void TimingTest()
+	{
+		UnitConverter unitConverter	= UnitFileLoading.LoadVersionTwoFile();
+
+		double value	= 1;
+		string inUnits	= "m";
+		string outUnits	= "ft";
+
+		Stopwatch stopwatch = Stopwatch.StartNew(); 
+
+		for (int i = 0; i < 10000000; i++)
+		{
+			UnitResult result = unitConverter.ConvertUnits(value, inUnits, outUnits, out double convertedValue);
+		}
+
+		stopwatch.Stop();
+		Debug.WriteLine("\n\nEllapsed millisecons: " + stopwatch.ElapsedMilliseconds);
+
 	}
 }
