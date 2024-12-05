702aaa7ad94af76a0ff52b14666393f42afa6259
lendres <lanceendres@gmail.com>
2024-12-05 09:33:36 -0600

    Update GlobalSuppressions.cs

--- Library/GlobalSuppressions.cs
+++ Library/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "The 'SubString' function is easier to read.", Scope = "namespaceanddescendants", Target = "~N:Thor.Units")]
 [assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Left for readability.", Scope = "namespaceanddescendants", Target = "~N:Thor.Units")]
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "<Pending>", Scope = "namespace", Target = "~N:Thor.Units")]
